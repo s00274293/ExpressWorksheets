0f214e67abf4fab1e50eb88672c414f941bb8f51
Kevin Staunton <s00274293@atu.ie>
2026-09-23 10:08:35 +0100

    completed ws2

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 2900;
 
 const app: Application = express();
 
-app.use('/api/v1/cars', carRoutes);
 app.use(express.json());
+app.use('/api/v1/cars', carRoutes);
 
 app.use((req, _res, next) => {  
     console.log(`${req.method} ${req.originalUrl}`);
